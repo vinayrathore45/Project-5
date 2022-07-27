1fbfd9312941bccdad0262bc5435fb516ba43805
Snehal <snehalkhandelwal13@gmail.com>
2022-07-27 12:46:45 +0530

    27th-3rd

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -155,26 +155,27 @@ const getUser = async function (req, res) {
 };
 
 const updatedUser = async function (req, res) {
+    try{
 
         let userId = req.params.userId
+
+        //check the userId is Valid or Not ?  
+        if (!mongoose.isValidObjectId(userId)) return res.status(400).send({ msg: "inavalid id format" })
+
         let data = req.body
         let { fname, lname, email, phone, password, address } = req.body
 
-        //check the userId is Valid or Not ?  
-        if (!ObjectId.isValid(userId)) {
-            return res.status(400).send({ status: false, msg: "userId is Invalid" });
-        }
 
         //check if body is empty or not ?
-        if (!Object.keys(data).length == 0) {
-            return res.status(400).send({ status: false, msg: "Noting to Update in Request from Body" });
+        if (Object.keys(data).length == 0) {
+            return res.status(400).send({ status: false, msg: "Noting to Update in the Request " });
         }
 
         //check if userid is present in Db or Not ? 
         let user = await userModel.findById(userId)
-        if (!user) return res.status(404).send({ status: false, msg: "userId is not present in DB" })
+        if (!user) return res.status(404).send({ status: false, msg: "No user found" })
 
-        var regEx = /^[a-zA-Z ]{2,100}$/
+        var regEx = /^[a-zA-Z ]{2,15}$/
 
         if (fname != null) {
             if (!regEx.test(fname)) return res.status(400).send({ status: false, msg: "fname text is invalid" });
@@ -185,12 +186,11 @@ const updatedUser = async function (req, res) {
         }
         //check the email unique or not 
         if (email != null) {
-            if (
-                !isValid(email)) return res.status(400).send({ status: false, message: "please enter email" })
+            if ( !isValid(email)) return res.status(400).send({ status: false, message: "please enter email" })
             if (!/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(email)) return res.status(400).send({ status: false, message: "please enter valid email" })
             let findEmail = await userModel.findOne({ email: email })
             if (findEmail) {
-                return res.status(400).send({ status: false, msg: "Email is Already Present in DB" })
+                return res.status(400).send({ status: false, msg: "This EmailId is already taken" })
             }
         }
 
@@ -204,49 +204,51 @@ const updatedUser = async function (req, res) {
 
 
 
-        // useing adreess validation
+        //  adreess validation
         if(address != null){
         const myAddress = JSON.parse(address)
-        // if (Object.keys(myAddress).length != 2) return res.status(400).send({ status: false, message: "Shipping or billing address is missing" })
+        if (Object.keys(myAddress).length == 0) return res.status(400).send({ status: false, message: "Shipping or billing address is missing" })
         //******shipping validation**************//
         if(myAddress.shipping != null){
         const shipping = myAddress.shipping
-        // if (Object.keys(shipping).length != 3) return res.status(400).send({ status: false, message: "Some shipping details is missing" })
+        if (Object.keys(shipping).length == 0) return res.status(400).send({ status: false, message: "Some shipping details is missing" })
         const { street, city, pincode } = shipping
         if(street != null){
             if (!isValid(street)) return res.status(400).send({ status: false, message: "please enter shipping street details" });
         }
         if(city != null){
             if (!isValid(city)) return res.status(400).send({ status: false, message: "please enter shipping city" });
+            if (!regEx.test(billing.city)) return res.status(400).send({ status: false, msg: " shipping city is invalid" });
         }
          if(pincode != null){
         if (typeof(pincode)!="number") return res.status(400).send({ status: false, message: "please enter shipping pincode" });
         if (!/^[1-9][0-9]{5}$/.test(pincode)) return res.status(400).send({ status: false, message: "please enter valid shippig pincode" })
-        }
+        }}
         //******billing validation**************//
-        if(billing != null){
+        if(myAddress.billing != null){
         const billing = myAddress.billing
-       
-        // if (Object.keys(billing).length != 3) return res.status(400).send({ status: false, message: "Some billing details is missing" })
-        if(billing.address != null){
+        if (Object.keys(billing).length == 0) return res.status(400).send({ status: false, message: "Some billing details is missing" })
+        if(billing.street != null){
             if (!isValid(billing.street)) return res.status(400).send({ status: false, message: "please enter billing street details" });
         }
         if(billing.city != null){
             if (!isValid(billing.city)) return res.status(400).send({ status: false, message: "please enter billing city" });
+            if (!regEx.test(billing.city)) return res.status(400).send({ status: false, msg: "billing city is invalid" });
         }
         if(billing.pincode != null){
         if (typeof(billing.pincode)!= "number") return res.status(400).send({ status: false, message: "please enter billing pincode" });
          if (!/^[1-9][0-9]{5}$/.test(billing.pincode)) return res.status(400).send({ status: false, message: "please enter valid billing pincode" })
-        }
-         req.body.address = myAddress
         }}
+         req.body.address = myAddress
+        }
+        // req.body.address = myAddress
         // image validation
         if (req.files.length > 0) {
             const files = req.files;
             if (!files && files.length > 0) return res.status(400).send({ status: false, message: "please enter profileImage" })
             const myFile = files[0]
             const fileType = myFile['mimetype'];
-            const validImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+            const validImageTypes = ['image/gif', 'image/jpeg', 'image/png' ,'image/jpg'];
             if (!validImageTypes.includes(fileType)) return res.status(400).send({ status: false, message: "Please enter valid image file" })
             //********uploading image to aws*******/
             const uploadImage = await file.uploadFile(myFile)
@@ -256,24 +258,23 @@ const updatedUser = async function (req, res) {
         //check if password is valid or not ?
         if(password != null){
         var passwordReg = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,15}$/;
-        if (!passwordReg.test(password)) {
-            return res.status(400).send({ status: false, msg: "pass is invalid(Minimum 8 and maximum 15 characters, at least one uppercase letter, one lowercase letter, one number and one special character Ex. Abc@123,abC%98,@abD34,1999$Sour" })
-        }
+        if (!passwordReg.test(password))  return res.status(400).send({ status: false, msg: "pass is invalid(Minimum 8 and maximum 15 characters, at least one uppercase letter, one lowercase letter, one number and one special character Ex. Abc@123,abC%98,@abD34,1999$Sour" })
+
         const bcryptPassword = await bcrypt.hash(password, 10)
         req.body.password = bcryptPassword
 
     }
 
         //if all condition are passed update data
-        let updatedUser = await userModel.findByIdAndUpdate(userId, data, { new: true })
+        let updatedUser = await userModel.findByIdAndUpdate(userId, req.body, { new: true })
 
         return res.status(200).send({ status: true, data: updatedUser });
-}}
-// }catch (err) {
-//         return res.status(500).send({ status: false, message: err.message })
-//     }
+}
+catch (err) {
+       return res.status(500).send({ status: false, message: err.message })
+    }
 
-// }
+ }
 
 
 
